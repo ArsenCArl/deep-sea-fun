6a0aeaf6fa5c19d72da46b88d670174883282a24
Arsenio <Carlosarsenio01@gmail.com>
2023-11-26 21:08:47 +0100

    Wave, CSS and bits
    - Fonts changed to use next/fonts local
    -opendyslexic font family
    - added first page as a test
    -Wave animation component (may have to redo it later)

--- deep-sea-portfolio/app/page.tsx
+++ deep-sea-portfolio/app/page.tsx
@@ -1,10 +1,15 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-200">
+    <div className="text-8xl">
       Arsenio
-    
+    </div>
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+
     </main>
   )
 }
---
+++ deep-sea-portfolio/app/under/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnderMain = () => {
+  return (
+    <div>UnderMain</div>
+  )
+}
+
+export default UnderMain
