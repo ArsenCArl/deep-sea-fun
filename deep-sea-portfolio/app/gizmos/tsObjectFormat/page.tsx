6561bfb7dfeffe0bb2ed02ddc8d5ad6bcfc7afca
Arsenio <Carlosarsenio01@gmail.com>
2023-12-06 12:55:05 +0100

    Automation Stash
    Decided to not pursue this automation longer but im putting the code in a branch. The reasons this automation is no longer part of the project is because it would automate less than 5 minutes of coding and to do anything relevant it would require some more days.
    
    Not really worth automating this behaviour upon analysis
    
    There is some cool code in the implementation so im putting it to a branch to not lose it, mostly the usage of a typescript dynamic key interface.

--- deep-sea-portfolio/app/gizmos/tsObjectFormat/page.tsx
+++ deep-sea-portfolio/app/gizmos/tsObjectFormat/page.tsx
@@ -1,16 +1,57 @@
-import React, { ReactElement } from "react";
+'use client'
+import React, { ReactElement, useEffect } from "react";
 import GizmoLayout from "../layout";
-import { ObjectFormater } from "@/app/tsInterfaces/gizmosData";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { removeDuplicates } from "@/app/utils/removeDuplicates";
+import { MinusCircleIcon } from "@heroicons/react/20/solid";
+
+interface objectDataType {
+  [key : string] : string 
+}
 
 const Page = () => {
+  const [fieldTemplate, setFieldTemplate] = React.useState<string[]>([]);
+  const [fieldJsonData, setFieldJsonData] = React.useState<objectDataType>();
+
+   
+  const handleInputChange = () => {
+  
+  }
+  const removeField = (fieldToRemove : string) => (e: React.MouseEvent) =>{
+    setFieldTemplate(fieldTemplate.filter(field => field !== fieldToRemove ))
+  }
+  
+    function addNewField() : void{
+      setFieldTemplate(removeDuplicates([...fieldTemplate,'placeholder']))
+    }
+
+
   return (
     <div>
-      <div>
-        <div>title</div>
-        <div>subtitle</div>
-      </div>
-      <div>
-        
+      <div className="">
+        <div className="">
+        <div>Template Selector</div>
+        <div>
+        {fieldTemplate.map((field) => (
+          <div id={field}>
+            {field}
+            <button onClick={removeField(field)}>
+                     <MinusCircleIcon className="w-5 h-5"></MinusCircleIcon>
+          </button> 
+          </div>
+
+        ))}
+        </div>
+        <div>
+           <button onClick={addNewField}>
+            <PlusIcon className="w-5 h-5"></PlusIcon>
+          </button> 
+          </div>
+        </div>
+        <div className="">
+          <div>JSON OBJECT Output</div>
+          <div>{JSON.stringify(fieldJsonData,)}</div>
+        </div>
       </div>
     </div>
   );
